dc96bb7c393af63151003e541454b8d12bc7d2d4
leandrovi <leandrofernandesvieira@gmail.com>
2020-01-03 14:45:30 -0200

    chore: add db.json to provide online json server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  // baseURL: 'http://localhost:3333',
+  baseURL: 'http://my-json-server.typicode.com/leandrovi/rocketshoes',
 });
 
 export default api;
