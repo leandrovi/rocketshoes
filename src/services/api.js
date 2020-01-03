f9ca7fca6fb7b09b1a06873009f9d22a9db269bc
leandrovi <leandrofernandesvieira@gmail.com>
2020-01-03 14:53:56 -0200

    fix: change online api to https

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:3333',
-  baseURL: 'http://my-json-server.typicode.com/leandrovi/rocketshoes',
+  baseURL: 'https://my-json-server.typicode.com/leandrovi/rocketshoes',
 });
 
 export default api;
